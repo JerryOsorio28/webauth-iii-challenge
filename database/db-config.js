11d75a0e136540617bbd07c09ff235836f393ce4
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-09-18 17:09:51 -0400

    Created migrations and seeds, working

---
+++ database/db-config.js
@@ -0,0 +1,5 @@
+const knex = require('knex');
+
+//import knex depencency
+//import knexFile
+//target development property on your knexFile
---
+++ database/migrations/20190918164239_Users.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('Users', tbl => {
+            tbl.increments(); // id auto increment
+
+            tbl
+                .string('username', 50)
+                .unique()
+                .notNullable()
+            tbl
+                .string('password', 50)
+                .notNullable()
+            tbl 
+                .string('department')
+                .notNullable()
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('users')
+};
